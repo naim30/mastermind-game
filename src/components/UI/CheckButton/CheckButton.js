f6e7f7fccba454248d3a6ec38a6081d82d33569c
Naimish Lukhi <lukhinaimish@gmail.com>
2021-09-04 12:31:20 +0530

    final commit

--- src/components/UI/CheckButton/CheckButton.js
+++ src/components/UI/CheckButton/CheckButton.js
@@ -2,13 +2,13 @@ import classes from "./CheckButton.module.css";
 
 const checkButton = (props) => {
   return (
-    <div
+    <button
       className={[
         classes.CheckButton,
         props.showCheck ? "" : classes.Disable,
       ].join(" ")}
       onClick={props.checkGuessHandler}
-    ></div>
+    ></button>
   );
 };
 
